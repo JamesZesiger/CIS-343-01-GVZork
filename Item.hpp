df9daf1a61622fc39141de3e2a40cc88dcc1e3da
Jared Bradley <jared@Jareds-Laptop>
2025-02-18 13:12:06 -0500

    Created main file with a test item and Item header file

---
+++ Item.hpp
@@ -0,0 +1,85 @@
+// Jared Bradley
+// 2/18/2025
+
+#ifndef         __HPP__ITEM__
+#define         __HPP__ITEM__
+
+#include <stdexcept>
+#include <iostream>
+#include <string>
+
+class Item {
+        public:
+            Item(std::string name, std::string description, int calories, float weight) {
+                if (name.empty()) {
+                    throw std::invalid_argument("Name cannot be blank.");
+                }
+
+                if (description.empty()) {
+                    throw std::invalid_argument("Description cannot be blank.");
+                }
+
+                if (calories < 0 || calories > 1000) {
+                    throw std::invalid_argument("Calories must be between 0 and 1000.");
+                }
+
+                if (weight < 0.0 || weight > 500.0) {
+                    throw std::invalid_argument("Weight must be between 0 and 500");
+                }
+
+                this->name = name;
+                this->description = description;
+                this->calories = calories;
+                this->weight = weight;
+            }
+
+            void setName(std::string name) {
+                this->name = name;
+            }
+
+            void setDescription(std::string description) {
+                this->description = description;
+            }
+
+            void setCalories(int calories) {
+                this->calories = calories;
+            }
+
+            void setWeight(float weight) {
+                this->weight = weight;
+            }
+
+            std::string getName() {
+                return this->name;
+            }
+
+            std::string getDescription() {
+                return this->description;
+            }
+
+            int getCalories() {
+                return this->calories;
+            }
+
+            float getWeight() {
+                return this->weight;
+            }
+
+            friend std::ostream& operator<<(std::ostream& os, const Item& item) {
+                os << item.name << " (" << item.calories << " calories) - " << item.weight << " lb - " << item.description;
+                return os;
+            } 
+
+
+
+        private:
+            std::string name;
+            std::string description;
+            int calories;
+            float weight;
+
+
+
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+// Jared Bradley
+// 2/18/2025
+#include "Item.hpp"
+#include <iostream>
+
+int main(int argc, char** argv){
+    Item nail("Rusty Nail", "A rusty nail (I hope you've had a tetanus shot)", 0, 1.0);
+    std::cout << nail << std::endl;
+}
