acc318b8c4cbab3660ccf7ac255de0be8cd9e765
Jared Bradley <jared@Jareds-Laptop>
2025-02-19 21:55:55 -0500

    Created NPC header and modified main to ensure creation works

---
+++ NPC.hpp
@@ -0,0 +1,72 @@
+// Jared Bradley
+// Feb 19th, 2025
+
+#ifndef         __HPP__NPC__
+#define         __HPP__NPC__
+
+#include <stdexcept>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class NPC {
+        public:
+            NPC(std::string name, std::string description){
+
+                if (name.empty()) {
+                    throw std::invalid_argument("Name cannot be blank.");
+                }
+
+                if (description.empty()) {
+                    throw std::invalid_argument("Description cannot be blank.");
+                }             
+
+                this->name = name;
+                this->description = description;
+                this->messageNum = 0;
+
+            }
+
+            void setName(std::string name) {
+                this->name = name;
+            }
+
+            void setDescription(std::string description) {
+                this->description = description;
+            }
+
+            std::string getName() {
+                return this->name;
+            }
+
+            std::string getDescription() {
+                return this->description;
+            }
+
+            std::string getMessage() {
+                if (messages.empty()) {
+                    return "They don't want to talk to you.";
+                }
+                std::string currentMessage = messages[messageNum];
+                messageNum = messageNum + 1;
+                return currentMessage;
+            }
+
+            friend std::ostream& operator<<(std::ostream& os, const NPC& npc) {
+                os << npc.name;
+                return os;
+
+            }
+
+
+
+        private:
+            std::string name;
+            std::string description;
+            std::vector<std::string> messages;
+            int messageNum;
+
+};
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 // Jared Bradley
 // 2/18/2025
+
 #include "Item.hpp"
+#include "NPC.hpp"
 #include <iostream>
 
 int main(int argc, char** argv){
     Item nail("Rusty Nail", "A rusty nail (I hope you've had a tetanus shot)", 0, 1.0);
     std::cout << nail << std::endl;
+
+    NPC Kevin("Kevin", "A normal Comp-Sci student at GV.");
+    std::cout << Kevin << std::endl;
 }
