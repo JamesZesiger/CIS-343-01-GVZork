a688ad61a6fd72de7796588d3d21ff629ca85a85
JamesZesiger <zesigeja@mail.gvsu.edu>
2025-02-22 15:10:20 -0500

    renamed functions and created basic create world function

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ void addInventory(std::vector<std::reference_wrapper<Item>> &inv, Item item, flo
     }
 }
 
-void removeInventory(std::vector<std::reference_wrapper<Item>> &inv, Item item, float capacity){
+void removeInventory(std::vector<std::reference_wrapper<Item>> &inv, Item &item, float capacity){
     for (int i = 0; i < inv.size(); i++){
         if (inv[i].get().getName() == item.getName()){
             inv.erase(inv.begin() + i);
@@ -41,12 +41,22 @@ void removeInventory(std::vector<std::reference_wrapper<Item>> &inv, Item item, 
     std::cout << item.getName() + " not found in inventory" << std::endl;
 }
 
-int main(int argc, char** argv){
+void play(std::vector<std::reference_wrapper<Item>> &itemList, std::vector<std::reference_wrapper<NPC>> &npcList, std::vector<std::reference_wrapper<Location>> &locationList, std::vector<std::reference_wrapper<Item>> &inventory, float &inventory_capacity){
+    addInventory(inventory, itemList[0], inventory_capacity);
+    printInventory(inventory);
+    addInventory(inventory, itemList[1], inventory_capacity);
+    removeInventory(inventory, itemList[0], inventory_capacity);
+    printInventory(inventory);
+}
+
+void createWorld(){
     //CREATE WORLD
+        std::vector<std::reference_wrapper<Item>> itemList;
+    std::vector<std::reference_wrapper<NPC>> npcList;
+    std::vector<std::reference_wrapper<Location>> locationList;
     //Create Items
     Item Apple("Apple", "A delicious red apple", 1, 0.5);
-    Item Bread("Bread", "A loaf of bread", 1, 1.0);
-    
+    Item Bread("Bread", "A loaf of bread", 1, 1.0); 
     //Create NPCs
     NPC Bob("Bob", "A normal Comp-Sci student at GV.");
     NPC Alice("Alice", "A sub-par Comp-Sci student at GV.");
@@ -54,28 +64,26 @@ int main(int argc, char** argv){
     NPC David("David", "A Comp-Sci student at GV who is always early.");
     NPC Eve("Eve", "A Comp-Sci student at GV who is always on time.");
     NPC Elf("Elf","A magical creature that is hungry.");
-    //Create Locations
-    
-    Location Library("Library", "There's lots of books in here.")
-
+    //Create Locations 
+    Location Library("Library", "There's lots of books in here.");
     //Create Inventory
     std::vector<std::reference_wrapper<Item>> inventory;
     float inventory_capacity = 0;
+    itemList.push_back(Apple);
+    itemList.push_back(Bread); 
+    npcList.push_back(Bob);
+    npcList.push_back(Alice);
+    npcList.push_back(Charlie);
+    npcList.push_back(David);
+    npcList.push_back(Eve);
+    npcList.push_back(Elf);
+    locationList.push_back(Library);
+    play(itemList, npcList, locationList, inventory, inventory_capacity);
+}
 
 
-    
-    
-    Item nail("Rusty Nail", "A rusty nail (I hope you've had a tetanus shot)", 0, 1.0);
-    std::cout << nail << std::endl;
-    addInventory(inventory, nail, inventory_capacity);
-    NPC Kevin("Kevin", "A normal Comp-Sci student at GV.");
-    std::cout << Kevin << std::endl;
-    printInventory(inventory);
-    
-    std::cout << Library<< std::endl;
-    addInventory(inventory, Apple, inventory_capacity);
-    addInventory(inventory, Bread, inventory_capacity);
-    removeInventory(inventory, nail, inventory_capacity);
-    printInventory(inventory);
 
+int main(int argc, char** argv){
+    createWorld();    
+    
 }
