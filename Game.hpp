7491036c1394cc567c2394a5f23f7773883f8039
connor-valley1 <valleyco@mail.gvsu.edu>
2025-02-24 14:41:36 -0500

    work on create_world and setup_commands

--- Game.hpp
+++ Game.hpp
@@ -21,6 +21,48 @@ class Game {
         }
 
         void create_world() {
+            //CREATE WORLD
+
+            //Create Items
+            Item Apple("Apple", "A delicious red apple", 1, 0.5);
+            Item Bread("Bread", "A loaf of bread", 1, 1.0);
+            Item LoMein("Lo Mein", "A cup of lo mein from Panda Express", 1, 2.0);
+
+            //Create NPCs
+            NPC Bob("Bob", "A normal Comp-Sci student at GV.");
+            NPC Alice("Alice", "A sub-par Comp-Sci student at GV.");
+            NPC Charlie("Charlie", "A Comp-Sci student at GV who is always late.");
+            NPC David("David", "A Comp-Sci student at GV who is always early.");
+            NPC Eve("Eve", "A Comp-Sci student at GV who is always on time.");
+            NPC Elf("Elf","A magical creature that is hungry.");
+            //Create Locations 
+            Location Library("Library", "There's lots of books in here.");
+            Location Kirkoff_one("Kirkoff Main Level", "Student Union, there is a restaurant, \
+                a coffee shop, and study spaces.\
+                 There are stairs to the second floor.");
+            Location Kirkoff_two("Kirkoff Second Floor", "There are large rooms and study spaces here.");
+            Location Padnos("Padnos Hall", "There are many classrooms here.");
+            Location Rec_Center("Recreation Center", "There is a pool, a gym, and basketball courts here.");
+
+            //Add items to locations
+            Library.addItem(Apple);
+            Kirkoff_one.addItem(LoMein);
+            Padnos.addItem(Bread);
+
+            //Add NPCs to locations
+            Library.addNPC(Bob);
+            Library.addNPC(Alice);
+            Kirkoff_one.addNPC(Elf);
+            Rec_Center.addNPC(Charlie);
+            Padnos.addNPC(David);
+            Padnos.addNPC(Eve);
+            
+            
+            this->locations.push_back(Library);
+            this->locations.push_back(Kirkoff_one);
+            this->locations.push_back(Kirkoff_two);
+            this->locations.push_back(Padnos);
+            this->locations.push_back(Rec_Center);
 
         }
 
@@ -52,16 +94,60 @@ class Game {
 
         }
 
+        void take(std::string target) {
+
+        }
+
         void show_help() {
         
         }
 
         Location random_location() {
-            
+
         }
 
         std::map<std::string, void(*)(std::vector<std::string>)> setup_commands() {
-            
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("quit", &Game::quit));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("exit", &Game::quit));
+
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("look", &Game::look));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("examine", &Game::look));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("inspect", &Game::look));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("search", &Game::look));
+
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("show_items", &Game::show_items));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("show", &Game::show_items));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("inventory", &Game::show_items));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("inv", &Game::show_items));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("items", &Game::show_items));
+
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("go", &Game::go));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("move", &Game::go));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("walk", &Game::go));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("travel", &Game::go));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("head", &Game::go));
+
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("give", &Game::give));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("drop", &Game::give));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("put", &Game::give));
+
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("meet", &Game::meet));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("who", &Game::meet));
+
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("talk", &Game::talk));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("speak", &Game::talk));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("ask", &Game::talk));
+
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("take", &Game::take));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("grab", &Game::take));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("pick", &Game::take));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("pickup", &Game::take));
+
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("help", &Game::show_help));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("?", &Game::show_help));
+            commands.insert(std::pair<std::string, void(*)(std::vector<std::string>)>("commands", &Game::show_help));
+
+            return this->commands;
         }
         
         void play() {
@@ -109,6 +195,7 @@ class Game {
         std::map<std::string, void(*)(std::vector<std::string>)> commands;
         std::vector<Item> items;
         std::vector<Location> locations;
+        int weight;
         Location current_location;
         int calories;
         bool gameOver;
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // Jared Bradley
 // 2/18/2025
 
+#include "Game.hpp"
 #include "Item.hpp"
 #include "Location.hpp"
 #include "NPC.hpp"
@@ -41,49 +42,11 @@ void removeInventory(std::vector<std::reference_wrapper<Item>> &inv, Item &item,
     std::cout << item.getName() + " not found in inventory" << std::endl;
 }
 
-void play(std::vector<std::reference_wrapper<Item>> &itemList, std::vector<std::reference_wrapper<NPC>> &npcList, std::vector<std::reference_wrapper<Location>> &locationList, std::vector<std::reference_wrapper<Item>> &inventory, float &inventory_capacity){
-    addInventory(inventory, itemList[0], inventory_capacity);
-    printInventory(inventory);
-    addInventory(inventory, itemList[1], inventory_capacity);
-    removeInventory(inventory, itemList[0], inventory_capacity);
-    printInventory(inventory);
-}
-
-void createWorld(){
-    //CREATE WORLD
-        std::vector<std::reference_wrapper<Item>> itemList;
-    std::vector<std::reference_wrapper<NPC>> npcList;
-    std::vector<std::reference_wrapper<Location>> locationList;
-    //Create Items
-    Item Apple("Apple", "A delicious red apple", 1, 0.5);
-    Item Bread("Bread", "A loaf of bread", 1, 1.0); 
-    //Create NPCs
-    NPC Bob("Bob", "A normal Comp-Sci student at GV.");
-    NPC Alice("Alice", "A sub-par Comp-Sci student at GV.");
-    NPC Charlie("Charlie", "A Comp-Sci student at GV who is always late.");
-    NPC David("David", "A Comp-Sci student at GV who is always early.");
-    NPC Eve("Eve", "A Comp-Sci student at GV who is always on time.");
-    NPC Elf("Elf","A magical creature that is hungry.");
-    //Create Locations 
-    Location Library("Library", "There's lots of books in here.");
-    //Create Inventory
-    std::vector<std::reference_wrapper<Item>> inventory;
-    float inventory_capacity = 0;
-    itemList.push_back(Apple);
-    itemList.push_back(Bread); 
-    npcList.push_back(Bob);
-    npcList.push_back(Alice);
-    npcList.push_back(Charlie);
-    npcList.push_back(David);
-    npcList.push_back(Eve);
-    npcList.push_back(Elf);
-    locationList.push_back(Library);
-    play(itemList, npcList, locationList, inventory, inventory_capacity);
-}
-
-
-
 int main(int argc, char** argv){
-    createWorld();    
-    
+
+    Game game;
+    game.create_world();
+    game.play();
+
+    return 0;
 }
